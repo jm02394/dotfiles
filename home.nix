996e1d6bf1fb505ceb0f6f350e599fd5e9096c36
reallygoodaccount1 <jonah@slackorama.com>
2022-10-09 21:48:44 -0700

    added random shit

--- home.nix
+++ home.nix
@@ -10,12 +10,15 @@
     userName = "reallygoodaccount1";
     userEmail = "jonah@slackorama.com";
   };
+
+  gtk.enable = true;
+  gtk.gtk3.extraConfig = {"gtk-application-prefer-dark-theme" = "true";};
   
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
              "discord" "vscode" "spotify" "spotify-unwrapped"
            ];
 
-  home.packages = with pkgs; [discord spotify neofetch pavucontrol];
+  home.packages = with pkgs; [discord spotify neofetch pavucontrol rustc cargo];
 
   home.stateVersion = "22.05";
   home.username = "user";
--- i3.nix
+++ i3.nix
@@ -15,8 +15,16 @@ in {
         "${mod}+m" = "move workspace to output right";
       };
 
+      window.hideEdgeBorders = "both";
+
+      workspaceAutoBackAndForth = true;
+
       terminal = "alacritty";
-      startup = [{command = "xrandr --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal";}];
+
+      startup = [
+        {command = "xrandr --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal";}
+        {command = "sh ${./mouseconfig.sh}"; always = true;}
+      ];
     };
   };
 }
